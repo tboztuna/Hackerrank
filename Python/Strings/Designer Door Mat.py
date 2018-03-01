b2e7335dfe9c5748760d3dd6d86187b48bc2aa11
Tolga Boztuna <tolga.boztuna@freebirdairlines.com>
2018-03-01 09:49:06 +0300

    Designer Door Mat solution added

---
+++ Python/Strings/Designer Door Mat.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    N, M = map(int,raw_input().split()) # More than 6 lines of code will result in 0 score. Blank lines are not counted.
+    for i in xrange(1,N,2):
+        print str(".|."*i).center(M, "-")
+    print "WELCOME".center(M, "-")
+    for i in xrange(N-2,-1,-2):
+        print str(".|."*i).center(M, "-")
