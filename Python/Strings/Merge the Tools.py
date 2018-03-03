b3ec2ef5d0c1183483b85df220bc0c063f8f4076
Tolga Boztuna <tolga.boztuna@freebirdairlines.com>
2018-03-03 14:56:07 +0300

    Merge the tools

---
+++ Python/Strings/Merge the Tools.py
@@ -0,0 +1,14 @@
+def merge_the_tools(string, k):
+    split_string = lambda str, n : [str[i:i+3] for i in range(0, len(str), n)]
+    splitted_str = split_string(string, k)
+    new_list = list()
+
+    for i in range(len(string)):
+       if stringi == i+1
+
+    print ""
+
+
+if __name__ == '__main__':
+    string, k = raw_input(), int(raw_input())
+    merge_the_tools(string, k)
